50e83e6a4481c1a5e6ce9c9a8a2134ae31395a1f
ThatPCNerd <spanky1994@gmail.com>
2014-02-13 21:10:02 -0500

    Test Bukkit Project

---
+++ src/bukkitplugin/pkg1/Bukkitplugin1.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bukkitplugin.pkg1;
+
+/**
+ *
+ * @author Karen-Jr
+ */
+public class Bukkitplugin1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
