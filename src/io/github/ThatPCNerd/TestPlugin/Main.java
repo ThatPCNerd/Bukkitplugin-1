72b7f7be35f9760c6af0f4f0cb7150843752c4a9
ThatPCNerd <spanky1994@gmail.com>
2014-02-13 21:16:40 -0500

    Main Class

---
+++ src/io/github/ThatPCNerd/TestPlugin/Main.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.github.ThatPCNerd.TestPlugin;
+import org.bukkit.plugin.java.JavaPlugin;
+/**
+ *
+ * @author Karen-Jr
+ */
+public class Main {
+    
+}
